1e113330329ec232ca4a40316845c3a3ac8bfe24
connerdouglass <conner.douglass@gmail.com>
2021-07-18 14:59:34 -0700

    Updated chatroom to add window message handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { TelegramAuthService } from './shared/services/telegram_auth.service';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit, OnDestroy {
+
+	private message_handler = (event: MessageEvent<any>) => {
+		const data = event.data;
+		if (!data || typeof data !== 'object') return;
+		if (data.type === 'auth') {
+			this.telegram_auth_service.storeUser(data.user);
+		}
+	};
+
+	public constructor(
+		private telegram_auth_service: TelegramAuthService,
+	) {}
+
+	public ngOnInit(): void {
+		window.addEventListener('message', this.message_handler);
+	}
+
+	public ngOnDestroy(): void {
+		window.removeEventListener('message', this.message_handler);
+	}
+
+}
--- src/app/chatroom/chatroom.component.ts
+++ src/app/chatroom/chatroom.component.ts
@@ -1,39 +1,21 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { distinctUntilChanged, map, shareReplay } from 'rxjs/operators';
-import { TelegramAuthService } from '../shared/services/telegram_auth.service';
 
 @Component({
 	selector: 'app-chatroom',
 	templateUrl: './chatroom.component.html',
 	styleUrls: ['./chatroom.component.scss']
 })
-export class ChatRoomComponent implements OnInit, OnDestroy {
+export class ChatRoomComponent {
 
 	public chat_room_identifier$ = this.route.params
 		.pipe(map(p => p.chat_room_identifier))
 		.pipe(distinctUntilChanged())
 		.pipe(shareReplay(1));
 
-	private message_handler = (event: MessageEvent<any>) => {
-		const data = event.data;
-		if (!data || typeof data !== 'object') return;
-		if (data.type === 'auth') {
-			this.telegram_auth_service.storeUser(data.user);
-		}
-	};
-
 	public constructor(
 		public route: ActivatedRoute,
-		private telegram_auth_service: TelegramAuthService,
 	) {}
-
-	public ngOnInit(): void {
-		window.addEventListener('message', this.message_handler);
-	}
-
-	public ngOnDestroy(): void {
-		window.removeEventListener('message', this.message_handler);
-	}
 
 }
